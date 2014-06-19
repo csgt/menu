aff2d5fe85bf005a439b812c0ab45e623c1fda48
root <root@serverdev.cs.local>
2014-06-19 09:39:14 -0600

    Primer commit

---
+++ src/Csgt/Menu/Menu.php
@@ -0,0 +1,96 @@
+<?php 
+/*************************************************************
+Laravel CS Core - No modificar 
+ver 1.1 - Se agrego el icono si esta configurado en la DB
+**************************************************************/
+namespace Csgt\Menu;
+use Config;
+use View;
+use AuthMenu;
+
+class Menu {
+	
+	protected $texto;
+	protected $matriz;
+
+	function generarNivel($aID, $aNivel) {
+		$primero = true;
+		foreach ($this->matriz[$aID] AS $item) {
+			switch ($aNivel) {
+				case 1:
+					$estiloUL = 'nav navbar-nav';
+					$estiloLI  = 'dropdown';
+					break;
+				case 2:
+					$estiloUL = 'dropdown-menu';
+					$estiloLI = 'dropdown-submenu';
+					break;
+				default:
+					$estiloUL = 'dropdown-menu';
+					$estiloLI = 'dropdown-submenu';
+					break;
+			}
+
+			if ($primero) $this->texto .= "<ul class='" . $estiloUL . "'>";			
+			$this->texto .= "<li" . ($item['ruta']==''?" class='" . $estiloLI . "'":"") . ">";
+			
+			$icon = "<span class=\"glyphicon " . $item['icono'] . "\"></span>";
+			
+			if ($item['ruta']=='') {
+				if ($aNivel==1) {
+					$this->texto .= "<a class='dropdown-toggle' data-toggle='dropdown' href='#'>";
+					if ($item['icono']<>'') $this->texto .= $icon;
+					$this->texto .= $item['titulo'] . "<b class='caret'></b></a>";
+				}
+				else {
+					$this->texto .= "<a tabindex='-1' href='#'>";
+					if ($item['icono']<>'') $this->texto .= $icon;
+					$this->texto .= $item['titulo'] . "</a>";
+				}
+				$this->generarNivel($item['menuid'], $aNivel+1);
+			}
+
+			elseif ($item['ruta']=='/') {
+				$this->texto .= "<a href='" . $item['ruta'] . "'>";
+				if ($item['icono']<>'') $this->texto .= $icon;
+				$this->texto .= $item['titulo'] . "</a>";
+			}
+
+			else {
+				$this->texto .= "<a href='/" . $item['ruta'] . "'>";
+				if ($item['icono']<>'') $this->texto .= $icon;
+			  $this->texto .= $item['titulo'] . "</a>";
+			}
+			
+			$this->texto .="</li>";
+			$primero=false;
+		}
+		if (!$primero)  $this->texto .= "</ul>";
+	}
+
+
+	function generarMenu($aMenuItems) {
+		$padreAnt = 'Primero';
+		$k=0;
+		
+		if (!is_array($aMenuItems)) return 'Error: Los elementos del menu deben venir en un array.';
+		
+		foreach($aMenuItems as $m) {
+			$m = (object)$m;
+			$padreID = (int)$m->padreid; //->padreid;
+			if ($padreID<>$padreAnt) $k=0;
+			foreach (Config::get('menu::campos') as $key=>$val) {
+				$this->matriz[$padreID][$k][$key] = $m->$val;
+			}
+
+			$k++;
+			$padreAnt = $padreID;
+		}
+
+		$this->generarNivel(0,1);
+		return View::make('menu::menutemplate')->with('elMenu', $this->texto);
+		
+		//return($this->texto);
+	}
+
+}
---
+++ src/Csgt/Menu/MenuServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Csgt\Menu;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class MenuServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('csgt/menu');
+		AliasLoader::getInstance()->alias('Menu','Csgt\Menu\Menu');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
