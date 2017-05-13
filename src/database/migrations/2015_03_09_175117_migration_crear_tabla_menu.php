edd9dd6f0d0aeb94617a111222ec20fc4cc46e6d
Jose Galindo <jgalindo@cs.com.gt>
2017-05-13 12:19:11 -0600

    Recuperar migration

---
+++ src/database/migrations/2015_03_09_175117_migration_crear_tabla_menu.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationCrearTablaMenu extends Migration {
+
+	public function up() {
+		Schema::create('authmenu', function(Blueprint $table) {
+			$table->increments('menuid');
+			$table->integer('padreid')->nullable()->unsigned();
+			$table->integer('modulopermisoid')->nullable()->unsigned();
+			$table->string('nombre',50);
+			$table->integer('orden');
+			$table->string('icono',50)->nullable();
+			$table->timestamps();
+
+			$table->foreign('modulopermisoid')->references('modulopermisoid')->on('authmodulopermisos')->onDelete('restrict')->onUpdate('cascade');
+			$table->foreign('padreid')->references('menuid')->on('authmenu')->onDelete('restrict')->onUpdate('cascade');
+
+		});
+	}
+
+	public function down() {
+		Schema::drop('authmenu');
+	}
+
+}
