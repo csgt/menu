0070c367ade761e956a481a5162476a3449439ec
Jose Galindo <jgalindo@cs.com.gt>
2018-01-30 17:53:35 -0600

    added route on migration

--- src/database/migrations/2015_03_09_175117_migration_crear_tabla_menu.php
+++ src/database/migrations/2015_03_09_175117_migration_crear_tabla_menu.php
@@ -10,9 +10,10 @@ class MigrationCrearTablaMenu extends Migration {
 			$table->increments('id');
 			$table->integer('parent_id')->nullable()->unsigned();
 			$table->integer('module_permission_id')->nullable()->unsigned();
-			$table->string('name', 50);
+			$table->string('name');
 			$table->integer('order');
-			$table->string('icon', 50)->nullable();
+			$table->string('route')->nullable();
+			$table->string('icon')->nullable();
 			$table->timestamps();
 
 			$table->foreign('module_permission_id')->references('id')->on('module_permissions')->onDelete('restrict')->onUpdate('cascade');
