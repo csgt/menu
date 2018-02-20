6d11874c90a04ffcca7b53adce2e82cde54f8c44
Jose Galindo <jgalindo@cs.com.gt>
2018-02-19 18:08:47 -0600

    changed table name

--- src/database/migrations/2015_03_09_175117_migration_crear_tabla_menu.php
+++ src/database/migrations/2015_03_09_175117_migration_crear_tabla_menu.php
@@ -3,27 +3,27 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MigrationCrearTablaMenu extends Migration {
+class MigrationCrearTablaMenu extends Migration
+{
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->nullable()->unsigned();
+            $table->integer('module_permission_id')->nullable()->unsigned();
+            $table->string('name');
+            $table->integer('order');
+            $table->string('route')->nullable();
+            $table->string('icon')->nullable();
+            $table->timestamps();
 
-	public function up() {
-		Schema::create('menu', function(Blueprint $table) {
-			$table->increments('id');
-			$table->integer('parent_id')->nullable()->unsigned();
-			$table->integer('module_permission_id')->nullable()->unsigned();
-			$table->string('name');
-			$table->integer('order');
-			$table->string('route')->nullable();
-			$table->string('icon')->nullable();
-			$table->timestamps();
+            $table->foreign('module_permission_id')->references('id')->on('module_permissions')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('parent_id')->references('id')->on('menu')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
 
-			$table->foreign('module_permission_id')->references('id')->on('module_permissions')->onDelete('restrict')->onUpdate('cascade');
-			$table->foreign('parent_id')->references('id')->on('menu')->onDelete('restrict')->onUpdate('cascade');
-
-		});
-	}
-
-	public function down() {
-		Schema::drop('menu');
-	}
-
+    public function down()
+    {
+        Schema::drop('menus');
+    }
 }
