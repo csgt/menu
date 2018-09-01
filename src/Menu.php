075832447795395cbf03bf4b458814e3a9418afe
Jose Galindo <jgalindo@cs.com.gt>
2018-08-31 19:32:06 -0600

    Parametro extra params para enviar info adicional a las rutas.

--- src/Menu.php
+++ src/Menu.php
@@ -1,49 +1,62 @@
-<?php namespace Csgt\Menu;
+<?php
+namespace Csgt\Menu;
 
-use Config, View, Exception, Request, Route, Session;
+use Route;
+use Config;
+use Session;
 
-class Menu {	
-	protected $texto;
+class Menu
+{
+    protected $texto;
 
-	function generarMenu($aCollection){
-		$this->generarNivel($aCollection, 0);
-		return $this->texto;
-	}
+    public function generarMenu($aCollection)
+    {
+        $this->generarNivel($aCollection, 0);
 
-	function generarNivel($aCollection, $aPadreId) {
-		$niveles = $aCollection->where('padreid', $aPadreId);
-		foreach ($niveles as $nivel) {
-			if(config('csgtmenu.usarLang')===true) {
-				$titulo = trans('csgtmenu::titulos.' . $nivel["nombre"]);
-			}
-			else {
-				$titulo = $nivel["nombre"];
-			}
+        return $this->texto;
+    }
 
-			$clase = '';
-			if ($nivel["ruta"] <> '') {
-				$clase = ((Session::get('menu-selected')==$nivel["ruta"])?'active':'');
-			}
-			$tieneHijos = $aCollection->where('padreid', $nivel["menuid"])->count()>0;
+    public function generarNivel($aCollection, $aPadreId)
+    {
+        $niveles = $aCollection->where('padreid', $aPadreId);
+        foreach ($niveles as $nivel) {
+            if (config('csgtmenu.usarLang') === true) {
+                $titulo = trans('csgtmenu::titulos.' . $nivel["nombre"]);
+            } else {
+                $titulo = $nivel["nombre"];
+            }
 
-			if ($tieneHijos) { //Tiene hijos
-				$this->texto .= '<li class="treeview ' . ($aPadreId==0?'treeview-padre':'') . '">';
-				$this->texto .= "<a href='#'>";
-					
-			}
-			else {
-				$this->texto .= "<li class='" . $clase . "'>";
-				$this->texto .= "<a href='" . route($nivel["ruta"]) . "'>";
-			}
-			if ($nivel["icono"] <>'') $this->texto .= "<i class='" . $nivel["icono"] . "'></i>";
-			$this->texto .= "<span>" . $titulo . "</span>";
-			if($tieneHijos) {
-				$this->texto .= "<span class='pull-right-container'><i class='fa fa-angle-left pull-right'></i></span>";
-			}
-			$this->texto .= "</a>" . ($tieneHijos?"<ul class='treeview-menu'>":"");
+            $clase = '';
+            if ($nivel["ruta"] != '') {
+                $clase = ((Session::get('menu-selected') == $nivel["ruta"]) ? 'active' : '');
+            }
+            $tieneHijos = $aCollection->where('padreid', $nivel["menuid"])->count() > 0;
 
-			$this->generarNivel($aCollection, $nivel["menuid"]);
-			$this->texto .= ($tieneHijos?"</ul>":"") . "</li>";
-		}
-	}
+            if ($tieneHijos) {
+                //Tiene hijos
+                $this->texto .= '<li class="treeview ' . ($aPadreId == 0 ? 'treeview-padre' : '') . '">';
+                $this->texto .= "<a href='#'>";
+
+            } else {
+                $this->texto .= "<li class='" . $clase . "'>";
+                if (!empty($nivel["params"])) {
+                    $this->texto .= "<a href='" . route($nivel["ruta"], $nivel["params"]) . "'>";
+                } else {
+                    $this->texto .= "<a href='" . route($nivel["ruta"]) . "'>";
+                }
+            }
+            if ($nivel["icono"] != '') {
+                $this->texto .= "<i class='" . $nivel["icono"] . "'></i>";
+            }
+
+            $this->texto .= "<span>" . $titulo . "</span>";
+            if ($tieneHijos) {
+                $this->texto .= "<span class='pull-right-container'><i class='fa fa-angle-left pull-right'></i></span>";
+            }
+            $this->texto .= "</a>" . ($tieneHijos ? "<ul class='treeview-menu'>" : "");
+
+            $this->generarNivel($aCollection, $nivel["menuid"]);
+            $this->texto .= ($tieneHijos ? "</ul>" : "") . "</li>";
+        }
+    }
 }
